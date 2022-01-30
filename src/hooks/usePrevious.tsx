16e5a689c00dd83c4cc079eba36590497b996ae3
faisalamrullah <faisal.amrulah@qoala.id>
2022-01-31 03:27:04 +0700

    first commit

--- src/hooks/usePrevious.tsx
+++ src/hooks/usePrevious.tsx
@@ -3,9 +3,9 @@ import React from 'react'
 function usePrevious<T>(value: T): T {
   const ref: any = React.useRef<T>()
   React.useEffect(() => {
-    ref.current = value //assign the value of ref to the argument
-  }, [value]) //this code will run when the value of 'value' changes
-  return ref.current //in the end, return the current ref value.
+    ref.current = value 
+  }, [value]) 
+  return ref.current 
 }
 
 export default usePrevious
